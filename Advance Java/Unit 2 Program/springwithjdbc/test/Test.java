bb7a12fe2bde2f7aac565b8a3e34fcedb3459d92
Maru Bhautik <91560701+Marubhautik@users.noreply.github.com>
2022-09-21 11:28:22 +0530

    yre
    yry

--- Advance Java/Unit 2 Program/springwithjdbc/test/Test.java
+++ Advance Java/Unit 2 Program/springwithjdbc/test/Test.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
     ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");  
       
     studentDao dao=(studentDao)ctx.getBean("edao");  
-    int status=dao.saveEmployee(new student(1,"bhautik",35000));  
+    int status=dao.saveEmployee(new student(2,"hitesh",36000));  
     System.out.println(status);  
           
     /*int status=dao.updateEmployee(new Employee(102,"Sonoo",15000)); 
